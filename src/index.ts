a2de15cb68ea5b80391c3df8e94393efd1bebe45
Wildann77 <boyblanco77@gmail.com>
2026-04-03 16:57:06 +0700

    push 6

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,17 @@ app.get("/todos",(req:Request,res:Response) => {
 app.get("/todos",(req:Request,res:Response) => {
     res.json(todos)
 })
+
+app.post("/todo",(req:Request,res:Response) => {
+
+    const {text} = req.body
+
+    const newTodo : Todo = {
+        id : Date.now(),
+        text,
+        done: false
+    }
+
+    todos.push(newTodo)
+    res.json(newTodo)
+})
