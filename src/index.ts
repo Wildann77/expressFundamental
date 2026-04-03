05c785b5cf613692a0befafb0837cadd3df89aeb
Wildann77 <boyblanco77@gmail.com>
2026-04-03 17:00:21 +0700

    push 11

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,32 @@
-import express, {type Request,type Response} from "express"
-import { todo } from "node:test"
+import express, { type Request, type Response } from "express"
 
 const app = express()
 app.use(express.json())
 
-
 type Todo = {
-    id : number,
-    text : string,
+    id: number,
+    text: string,
     done: boolean
-} 
+}
 
-let todos : Todo[] = []
+let todos: Todo[] = []
 
-
-app.get("/todos",(req:Request,res:Response) => {
+// GET: Semua todo
+app.get("/todos", (req: Request, res: Response) => {
     res.json(todos)
 })
 
-app.get("/todos",(req:Request,res:Response) => {
-    res.json(todos)
-})
+// POST: Tambah todo
+app.post("/todo", (req: Request, res: Response) => {
+    const { text } = req.body
 
-app.post("/todo",(req:Request,res:Response) => {
+    if (!text) {
+        return res.status(400).json({ error: "Property 'text' is required" })
+    }
 
-    const {text} = req.body
-
-    const newTodo : Todo = {
-        id : Date.now(),
-        text,
+    const newTodo: Todo = {
+        id: Date.now(),
+        text: text,
         done: false
     }
 
@@ -36,27 +34,28 @@ app.post("/todo",(req:Request,res:Response) => {
     res.json(newTodo)
 })
 
-app.put("/todo/:id", (req:Request,res:Response) => {
-
+// PUT: Update status atau teks
+app.put("/todo/:id", (req: Request, res: Response) => {
     const id = Number(req.params.id)
-    const {text} = req.body
-    todos = todos.map(todo => todo.id === id ? {...todo,text:text,done:!todo.done} : todo)
+    const { text } = req.body
 
-    res.json({message : "Todo updated"})
-})
-
-app.delete("/todo/:id",(req:Request,res:Response) => {
-    const id = Number(req.params.id)
-
-    todos = todos.filter(todo => 
-        todo.id !== id
+    todos = todos.map(todo => 
+        todo.id === id 
+            ? { ...todo, text: text || todo.text, done: !todo.done } 
+            : todo
     )
 
-    res.json({message : "Todo Deleted"})
+    res.json({ message: "Todo updated" })
 })
 
-const PORT = 3000   
+// DELETE: Hapus todo
+app.delete("/todo/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    todos = todos.filter(todo => todo.id !== id)
+    res.json({ message: "Todo Deleted" })
+})
 
+const PORT = 3000
 app.listen(PORT, () => {
-    console.log(`server berjalan pada port ${PORT}`)
+    console.log(`Server berjalan pada port ${PORT}`)
 })
