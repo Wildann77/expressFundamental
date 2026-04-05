5dac95715657ea05bbd3d69b26e7bf84a84670fb
Wildann77 <boyblanco77@gmail.com>
2026-04-05 13:26:35 +0700

    database_orm4

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/todos", todoRoutes)
+
+app.use((req:any,res:any,next:any,err:any) => {
+    res.status(400).json({
+        message : err.message
+    })
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
