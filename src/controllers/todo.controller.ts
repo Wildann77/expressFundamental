84b9b55d040a8badf70bc94bd2c28d45d32db8d2
Wildann77 <boyblanco77@gmail.com>
2026-04-05 14:25:02 +0700

    database_orm6

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,58 @@
+import { type Request, type Response, type NextFunction } from "express"
+import { todo } from "node:test"
+
+export const geAllTodos = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const todos = await todoService.getTodos()
+        res.json(todos)
+    } catch (error) {
+        next(error)
+
+    }
+}
+
+export const createTodo = async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        const { text } = req.body
+
+        if (!text) {
+            return res.json({
+                message: "Text is required"
+            })
+        }
+
+        const todo = await todoService.createTodo(text)
+        res.json(todo)
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+export const updateTodo = async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const id = Number(req.params.id)
+
+        const { text } = req.body
+
+        const todo = await todoService.updateTodo(id, text)
+        res.json(todo)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const deleteTodo = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number(req.params.id)
+        await todoService.deleteTodo(id)
+        res.json({ message: "Deleted" })
+    } catch (err) {
+        next(err)
+    }
+}
